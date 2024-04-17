aa28cd8eabfbd89fdf583a3db57cf878fc3aa850
Bal Gopal Lal Shrestha <03bgls25@gmail.com>
2024-04-17 14:53:51 +0545

    initial commit

---
+++ app/api/forecast/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest){
+    const { searchParams } = new URL(req.url);
+    const address = searchParams.get('address');
+    const latitude = searchParams.get('lat');
+    const longitude = searchParams.get('lon');
+
+    let url = "";
+    if(address){
+        url = `${process.env.NEXT_PUBLIC_API}/forecast?q=${address}&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric`;
+    } else {
+        url = `${process.env.NEXT_PUBLIC_API}/forecast?lat=${latitude}&lon=${longitude}&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric`;
+    }
+    const res = await fetch(url);
+    const data = await res.json();
+    return NextResponse.json({ data });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,223 @@
+"use client"
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { OutlinedInput, InputAdornment } from '@mui/material';
+import { LocationOnOutlined, AirOutlined, WaterDropOutlined, FilterDramaOutlined, CompareArrowsOutlined } from '@mui/icons-material';
+
+function getCurrentDateTime(offsetSeconds: number) {
+  const systemOffsetSeconds = new Date().getTimezoneOffset() * -60;
+  //console.log(systemOffsetSeconds);
+  if (offsetSeconds == systemOffsetSeconds) {
+    const currentDate = new Date();
+    //console.log(currentDate);
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    return {
+      year,
+      month,
+      day,
+      hours,
+      minutes,
+      seconds
+    }
+  }
+  else{
+    const timezoneOffsetMinutes = offsetSeconds / 60;
+    const currentDate = new Date();
+    currentDate.setMinutes(currentDate.getMinutes() + timezoneOffsetMinutes);
+
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+    return {
+      year,
+      month,
+      day,
+      hours,
+      minutes,
+      seconds
+    }
+  }
+}
+
+function formatTimestampToHourMinute(timestamp: number) {
+  const milliseconds = timestamp * 1000;
+  const date = new Date(milliseconds);
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const formattedHour = String(hour).padStart(2, '0');
+  const formattedMinute = String(minute).padStart(2, '0');
+  const formattedTime = `${formattedHour}:${formattedMinute}`;
+  return formattedTime;
+}
 
 export default function Home() {
+  const [timeData, setTimeDate] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchDataByCityName(cityName: string) {
+    try {
+      const res = await fetch(`http://localhost:3000/api/weather?address=${cityName}`);
+      const jsonData = (await res.json()).data;
+      if(jsonData.cod == 200) {
+        const timeData = getCurrentDateTime(jsonData.timezone);
+        setTimeDate(timeData);
+        setWeatherData(jsonData);
+        setLoading(false);
+      }else{
+        setLoading(true);
+      }
+      
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function fetchDataByCoordinates(latitude: number, longitude: number) {
+    try {
+      const res = await fetch(`http://localhost:3000/api/weather?lat=${latitude}&lon=${longitude}`);
+      const jsonData = (await res.json()).data;
+      const timeData = getCurrentDateTime(jsonData.timezone);
+      setTimeDate(timeData);
+      setWeatherData(jsonData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function fetchHourlyDataByCityName(cityName: string){
+    try {
+      const res = await fetch(`http://localhost:3000/api/forecast?address=${cityName}`);
+      const jsonData = (await res.json()).data;
+      if(jsonData.cod == 200) {
+        setForecastData(jsonData);
+        setLoading(false);
+      }else {
+        setLoading(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function fetchHourlyDataByCoordinates(latitude: number, longitude: number){
+    try {
+      const res = await fetch(`http://localhost:3000/api/forecast?lat=${latitude}&lon=${longitude}`);
+      const jsonData = (await res.json()).data;
+      setForecastData(jsonData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function searchLocation(cityName){
+    await fetchDataByCityName(cityName);
+    await fetchHourlyDataByCityName(cityName);
+  }
+
+  useEffect(() => {
+    if("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          fetchDataByCoordinates(latitude, longitude);
+          fetchHourlyDataByCoordinates(latitude, longitude);
+        }, (err) => {
+          console.log(err)
+        }
+      )
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="max-h-full p-5">
+      <div className="bg-white/90 shadow-xl w-full rounded-lg flex flex-col container h-fit ml-auto mr-auto">
+        <div className="flex justify-between items-center p-4">
+          <div className="flex justify-center items-center">
+            <Image src="/img/logo.png" alt="Weather App" width={40} height={40} className="mr-4" />
+            <h1 className="text-zinc-950 font-semibold">Open Weather App</h1>
+          </div>
+          <div className="w-1/4">
+            <OutlinedInput className="w-full" startAdornment={
+              <InputAdornment position="start">
+                <LocationOnOutlined />
+              </InputAdornment>
+            } 
+            placeholder="Search Location Here..."
+            onChange={(e) => { 
+              searchLocation(e.target.value)
+            }}
             />
-          </a>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="grid grid-cols-3 gap-3 container ml-auto mr-auto my-10">
+        {weatherData && !loading ? (
+          <div>
+              <h1 className="text-white text-xl font-bold mt-2">{weatherData.name}</h1>
+              <h2 className="text-white text-lg font-light mt-2">
+                {timeData.month} {timeData.day}, {timeData.year}
+              </h2>
+              <h3 className="text-white text-3xl font-medium mt-4">
+                {timeData.hours}:{timeData.minutes}
+              </h3>
+              <div className="flex justify-between items-center my-6">
+                <h4 className="text-white text-7xl font-semibold">
+                  {weatherData.main.temp}&deg;c
+                </h4>
+                <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather App" width={75} height={75} className="" />            
+              </div>
+              <div className="grid grid-cols-4 gap-2 mt-10">
+                <div className="bg-white/80 shadow-xl rounded p-4 flex flex-col items-center">
+                  <AirOutlined className="text-slate-600" />
+                  <label className="font-light text-xs mt-2">Wind</label>
+                  <label>{weatherData.wind.speed} m/sec</label>
+                </div>
+                <div className="bg-white/80 shadow-xl rounded p-4 flex flex-col items-center">
+                  <WaterDropOutlined className="text-slate-600" />
+                  <label className="font-light text-sm mt-2">Humidity</label>
+                  <label>{weatherData.main.humidity}%</label>
+                </div>
+                <div className="bg-white/80 shadow-xl rounded p-4 flex flex-col items-center">
+                  <FilterDramaOutlined className="text-slate-600" />
+                  <label className="font-light text-sm mt-2">Clouds</label>
+                  <label>{weatherData.clouds.all}%</label>
+                </div>
+                <div className="bg-white/80 shadow-xl rounded p-4 flex flex-col items-center">
+                  <CompareArrowsOutlined className="text-slate-600" />
+                  <label className="font-light text-sm mt-2">Pressure</label>
+                  <label>{weatherData.main.pressure} hPa</label>
+                </div>
+              </div>
+          </div>
+        ):(
+          <p>No city name available</p>
+        )
+        }
+        <div className="grid grid-cols-10 col-span-2">
+          {forecastData && !loading ? (
+            // <p>{forecastData.list.length}</p>
+            forecastData.list.length > 0 && forecastData.list.map((item, index) => (
+              <div key={index} className="bg-white/80 shadow-xl rounded col-span-2 m-2 p-2">
+                <p className="text-center font-light">{formatTimestampToHourMinute(item.dt)}</p>
+                <p className="text-center font-semibold text-lg">{item.main.temp}&deg;c</p>
+              </div>
+            )) 
+          ):(
+            <p></p>
+          )
+          }
+        </div>
       </div>
     </main>
   );
